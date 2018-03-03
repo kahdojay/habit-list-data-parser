0e6af70f7728c7d301d51210e53ba76e18156925
Don J <kahdojay@gmail.com>
2018-03-03 11:21:00 -0500

    Remove unnecessary .getUTCDate calls - breaks cross-month comparisons

--- parse-data.js
+++ parse-data.js
@@ -1,7 +1,7 @@
 // require three args: json file, start date, and end date
 if (
-  process.argv[2] === undefined || 
-  process.argv[3] === undefined || 
+  process.argv[2] === undefined ||
+  process.argv[3] === undefined ||
   process.argv[4] === undefined) {
     console.log('Error: invalid arguments');
     console.log('usage: node parse-data.js <habitdata.json> <startDate (mm/dd)> <endDate (mm/dd)> to get habit count for dates inclusive');
@@ -23,8 +23,8 @@ console.log('-------------------')
 habitData.forEach(function(habitObj) {
   let completions = []
   habitObj.completed.forEach(function(date) {
-    let completionDate = new Date(date).getUTCDate()
-    if (completionDate >= new Date(startDate.getUTCDate()) && completionDate <= new Date(endDate.getUTCDate())) {
+    let completionDate = new Date(date)
+    if (completionDate >= new Date(startDate) && completionDate <= new Date(endDate)) {
       completions.push(completionDate)
     }
   })
